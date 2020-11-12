b168b8188d538472c186b5fc5a49bb573bfdf40c
v_fulinoliu <v_fulinoliu@tencent.com>
2020-11-12 10:46:45 +0800

    20201112

---
+++ chrome_extension/ext/popup.js
@@ -0,0 +1,16 @@
+let changeColor = document.getElementById('changeColor');
+
+  // chrome.storage.sync.get('color', function(data) {
+  //   changeColor.style.backgroundColor = data.color;
+  //   changeColor.setAttribute('value', data.color);
+  // });
+
+changeColor.onclick = function(element) {
+    // let color = element.target.value;
+    let color = 'red'
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: `document.getElementById('p').value='1256'`});
+    });
+  };
